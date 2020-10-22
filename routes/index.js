5c09aa4747ec684f026b44fc39e95dbece4e76be
bearlogin <bearlogin@mail.ru>
2020-10-23 00:17:53 +0300

    post

--- routes/index.js
+++ routes/index.js
@@ -5,12 +5,12 @@ const TelegramBot = require('node-telegram-bot-api');
 router.post('/webhook', async function (req, res, next) {
     const bot = new TelegramBot(process.env.BOT_TOKEN);
     console.log(JSON.stringify(req.body));
-    const issue = req.body.data.issue;
+    const data = req.body;
+    const event = req.body.event;
     const message = `
-Issue: ${issue.title}    \n
-Project: ${issue.project.name} \n
-userCount:  ${issue.userCount} \n
-link:  ${issue.userCount} \n
+Issue: ${event.title}    \n
+Project: ${data.project_slug} \n
+link:  ${data.url} \n
     `
     await bot.sendMessage(process.env.CHAT_ID, message, {
         parse_mode: "HTML",
